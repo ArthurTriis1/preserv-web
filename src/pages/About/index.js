415c665d9ff76a59e55b9403d4695f6358bb6cb2
aluno <arthurfelandrade@gmail.com>
2020-06-18 11:51:20 -0300

    ajuste do form 3

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FiArrowLeft } from "react-icons/fi";
 import { AiFillGithub, AiFillInstagram, AiFillBehanceSquare } from 'react-icons/ai'
@@ -7,6 +7,32 @@ import './style.css'
 
 const About = () => {
     const history = useHistory();
+    const [form, setForm] = useState({
+        email: '',
+        message: ''
+    })
+
+    const formChange = e => {
+        setForm({ ...form, [e.target.name]: e.target.value }) 
+    };
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+      }
+
+    const handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", form })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+  
+        e.preventDefault();
+      };
     
     return (
         <div className="containerAbout">
@@ -17,6 +43,7 @@ const About = () => {
                 <img src={logo} alt="Preserv" className="logoHeaderAbout"/>
                 <h4 className="infoHeaderPreserv">Localize preservativos e serviços gratuitos para você!</h4>
             </header>
+            
             <div className="containerInformations">
                 <h5>O PRESERV TEM COMO OBJETIVO PRINCIPAL  LOCALIZAR E EXIBIR PONTOS DE DISTRIBUIÇÃO DE PRESERVATIVOS. TAMBÉM TEM FOCO EM MOSTRAR OS POSTOS DE SAÚDE QUE REALIZAM TESTES E TRATAMENTOS DE DST.</h5>
                 
@@ -42,14 +69,15 @@ const About = () => {
                     </a>
                 </div>
             </div>
-            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" className="containerFeedback">
+            
+            <form onSubmit={handleSubmit} className="containerFeedback">
                 <input type="hidden" name="form-name" value="contact" />
                 
                 <h2>DÚVIDAS OU SUGESTÕES?</h2>
   
-                <input type="email" name="email" placeholder="SEU EMAIL"/>
+                <input type="email" name="email" placeholder="SEU EMAIL" onChange={formChange}/>
 
-                <textarea type="text" name="message" placeholder="TEXTO" />
+                <textarea type="text" name="message" placeholder="TEXTO" onChange={formChange}/>
 
                 <button type="submit">ENVIAR</button>
             </form>
