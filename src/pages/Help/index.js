c86c972eda045a33e18251efbd45583e564ae43a
aluno <arthurfelandrade@gmail.com>
2020-06-22 19:21:37 -0300

    Adicionadas nosvas ajudas na pagina help

--- src/pages/Help/index.js
+++ src/pages/Help/index.js
@@ -28,6 +28,29 @@ const Help = () => {
 
                 <h2>Como saber se camisinha furou?</h2>
                 <Info information="ENCHA O PRESERVATIVO COM ÁGUA OU AR E CHEQUE SE não existe vazamentos"/>
+
+                <h2>Guardar camisinha na carteira estraga?</h2>
+                <Info information="Depende do tempo que você guarda. Todos os preservativos têm data de validade. Evite colocar perto de objetos pontiagudos ou que possam danificar a embalagem. Não exponha ao calor, umidade ou luzes fortes."/>
+
+                <h2>Usar uma camisinha por cima da outra protege mais?</h2>
+                <Info information="Não, porque o atrito entre os preservativos estimula rasgos e fissuras."/>
+
+                <h2>Existem tamanhos diferentes de camisinha?</h2>
+                <Info information="As camisinhas são altamente elásticas, mas o que muda é a largura do produto e não comprimento."/>
+
+                <h2>É possível usar camisinha debaixo d'água?</h2>
+                <Info information="Sim, mas é importante que coloque antes de entrar na água. Lembre que se a água conter cloro, óleo, sabão ou espuma para banho, pode afetar o látex."/>
+
+                <h2>Pessoas com alergia a látex podem usar camisinha?</h2>
+                <Info information="Existem preservativos no mercado que são feitas de silicone. Até mesmo a camisinha feminina é de um material pouco alergênico. O ideal é buscar as melhores alternativas para você."/>
+
+                <h2>Posso usar lubrificantes com camisinha?</h2>
+                <Info information="Sim, mas desde que o lubrificante seja à base de água ou silicone. Os que são à base de óleo, como óleo de bebê ou vaselina, podem reduzir a firmeza do látex."/>
+
+                <h2>A camisinha diminui o prazer?</h2>
+                <Info information="Algumas pessoas acham que interfere na sensação, mas no mercado existem camisinhas ultra finas e outras opções que estimulam o prazer do casal. Vale a pena pesquisar a melhor opção para você."/>
+            
+            
             </div>
         </div>
     )
