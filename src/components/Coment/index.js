155b6eebab2507d6e1180f943262510f8e3f3840
Arthur Andrade <arthurfelandrade@gmail.com>
2021-03-22 21:08:56 -0300

    Adicio funcionalidade de click
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/components/Coment/index.js
+++ src/components/Coment/index.js
@@ -1,14 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { AiFillLike, AiOutlineLike } from "react-icons/ai";
 
-import './style.css';
+import "./style.css";
 
-const Coment = ({username, coment}) => {
+const Coment = ({ comment, likeClick }) => {
+  const [thisComment, setThisComment] = useState(comment);
+  const [liked, setLiked] = useState(false);
+
+  const clickLikeHandle = (clickValue) => {
+    setLiked(clickValue);
+
+    const attr = clickValue ? +1 : -1;
+
+    if (clickValue) {
+      addIdStorage();
+    } else {
+      removeIdStorage();
+    }
+
+    const newComment = {
+      ...thisComment,
+      likes: thisComment.likes + attr,
+    };
+
+    likeClick(newComment);
+
+    setThisComment(newComment);
+  };
+
+  useEffect(() => {
+    const likedComments = getLikedComments();
+    const isLiked = likedComments.includes(comment.id);
+    setLiked(isLiked);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getLikedComments = () => {
+    let likedComments;
+
+    try {
+      likedComments = JSON.parse(localStorage.getItem("likedComments"));
+    }catch (e){
+      likedComments = [];
+    }
+
+    if (likedComments && Array.isArray(likedComments)) {
+      return likedComments;
+    } else {
+      return [];
+    }
+  };
+
+  const setLikedComments = (arrayComments) => {
+    localStorage.setItem("likedComments", JSON.stringify(arrayComments));
+  };
+
+  const removeIdStorage = () => {
+    const likedComments = getLikedComments();
+    const removedLikedComments = likedComments.filter(
+      (commentId) => commentId !== thisComment.id
+    );
+    setLikedComments(removedLikedComments);
+  };
+
+  const addIdStorage = () => {
+    const likedComments = getLikedComments();
+    likedComments.push(thisComment.id);
+    setLikedComments(likedComments);
+  };
+
   return (
-  <div className="comentContainer">
-      <h4>{username}</h4>
-      <p>{coment}</p>
-  </div>
-  )
-}
+    <div className="comentContainer">
+      <div>
+        <h4>{thisComment.username}</h4>
+        <p>{thisComment.comment}</p>
+        <span>{thisComment.likes} curtidas</span>
+      </div>
+      {liked ? (
+        <AiFillLike size={30} onClick={() => clickLikeHandle(!liked)} />
+      ) : (
+        <AiOutlineLike size={30} onClick={() => clickLikeHandle(!liked)} />
+      )}
+    </div>
+  );
+};
 
 export default Coment;
--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,114 +1,111 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import HeaderDetails from '../../components/HeaderDetails'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import HeaderDetails from "../../components/HeaderDetails";
 
-import fone from '../../assets/fone.png'
-import address from '../../assets/address.png'
-import name from '../../assets/name.png'
-import interrogation from '../../assets/interrogation.png'
-import service from '../../assets/service.png'
-import time from '../../assets/time.png'
+import fone from "../../assets/fone.png";
+import address from "../../assets/address.png";
+import name from "../../assets/name.png";
+import interrogation from "../../assets/interrogation.png";
+import service from "../../assets/service.png";
+import time from "../../assets/time.png";
 
-import  Coment  from '../../components/Coment';
+import Coment from "../../components/Coment";
 
-import './styles.css'
-import InputComent from '../../components/InputComent';
-import apiPreserv from '../../services/apiPreserv';
-import Modal from '../../components/Modal';
+import "./styles.css";
+import InputComent from "../../components/InputComent";
+import apiPreserv from "../../services/apiPreserv";
+import Modal from "../../components/Modal";
 
 export default function LocationDetails() {
+  const location = useLocation();
 
-    const location = useLocation();
-    
-    const [coments, setComents] = useState([]);
-    const [showSubmitComment, setShowSubmitComment] = useState(false);
+  const [coments, setComents] = useState([]);
+  const [showSubmitComment, setShowSubmitComment] = useState(false);
 
-    const marker = {
-        ...location.state.marker, 
-        endereço: location.state.marker.bairro+ ", " + location.state.marker["endereço"]
-    }
+  const marker = {
+    ...location.state.marker,
+    endereço:
+      location.state.marker.bairro + ", " + location.state.marker["endereço"],
+  };
 
-    useEffect(() => {
-        getComments();
+  useEffect(() => {
+    getComments();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  }, []);
 
-    const submitComent = (e) => {
-        apiPreserv.post('/new_comment', {
-            ...e,
-            place: location.state.marker._id
-        })
-        .then(({ data }) => { 
-            getComments();
-            setShowSubmitComment(true);
-        })
-    }
+  const submitComent = (e) => {
+    apiPreserv
+      .post("/new_comment", {
+        ...e,
+        place: location.state.marker._id,
+      })
+      .then(({ data }) => {
+        getComments();
+        setShowSubmitComment(true);
+      });
+  };
 
-    const getComments = () => {
-        apiPreserv.get('/comments/place/' + location.state.marker._id)
-            .then(({ data }) => { 
-                setComents(data.comments)
-            })
-    }
+  const getComments = () => {
+    apiPreserv
+      .get("/comments/place/" + location.state.marker._id)
+      .then(({ data }) => {
+        setComents(data.comments);
+      });
+  };
 
-    const infos = [
-        {icon: name , att: "nome_oficial"},
-        {icon: fone, att: "fone"},
-        {icon: address, att: "endereço"},
-        {icon:time, att: "horario"},
-        {icon:service, att: "tipo_servico"},
-        {icon: interrogation, att: "como_usar"}, 
-    ]
+  const likedComment = (comment) => {
+    apiPreserv.put("/comments/update/" + comment.id, comment)
+  };
 
-    return (
-        <>
-            { 
-                showSubmitComment  && 
-                <Modal 
-                    title="OBRIGADO!"
-                    text="SEU COMENTARIO FOI ENVIADO COM SUCESSO!" 
-                    close={() => setShowSubmitComment(false)}
-                /> 
+  const infos = [
+    { icon: name, att: "nome_oficial" },
+    { icon: fone, att: "fone" },
+    { icon: address, att: "endereço" },
+    { icon: time, att: "horario" },
+    { icon: service, att: "tipo_servico" },
+    { icon: interrogation, att: "como_usar" },
+  ];
+
+  return (
+    <>
+      {showSubmitComment && (
+        <Modal
+          title="OBRIGADO!"
+          text="SEU COMENTARIO FOI ENVIADO COM SUCESSO!"
+          close={() => setShowSubmitComment(false)}
+        />
+      )}
+      <div className={"container"}>
+        <HeaderDetails isInformation={true} />
+
+        <div className={"informationsContainer"}>
+          {infos.map((info) => {
+            if (String(marker[info.att]).length > 0) {
+              return (
+                <div key={String(info.att)} className={"groupInformation"}>
+                  <img alt="Logo" src={info.icon} className={"caracterLogo"} />
+                  <p className={"caracterText"}>{marker[info.att]}</p>
+                </div>
+              );
             }
-            <div className={"container"}> 
-                <HeaderDetails isInformation={true}/>
+            return null;
+          })}
+        </div>
 
-                <div className={"informationsContainer"}>
-                    {
-                        infos.map(info => {
-                            if(String(marker[info.att]).length > 0){
-                                return (
-                                    <div key={String(info.att)} className={"groupInformation"}>
-                                        <img alt="Logo" src={info.icon} className={"caracterLogo"}/>
-                                        <p className={"caracterText"}>{marker[info.att]}</p>
-                                    </div>
-                                )
-                            }
-                            return null;
-                        })
-                    }
-                </div> 
-                    
-                <section className="comentsSpace">
-                    <div>
-                        <InputComent submit={submitComent}/>
-                        {
-                            !!coments.length && 
-                            <h2>Comentarios:</h2>
-                        }
-                        {
-                            coments.map(coment => (
-                                <Coment 
-                                    key={coment.id}
-                                    username={coment.username} 
-                                    coment={coment.comment}
-                                />
-                            ))
-                        }
-                    </div>
-
-                </section>
-            </div>
-        </>
-    );
+        <section className="comentsSpace">
+          <div>
+            <InputComent submit={submitComent} />
+            {!!coments.length && <h2>Comentarios:</h2>}
+            {coments.map((comment) => (
+              <Coment
+                key={comment.id}
+                comment={comment}
+                likeClick={(c) => likedComment(c)}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
